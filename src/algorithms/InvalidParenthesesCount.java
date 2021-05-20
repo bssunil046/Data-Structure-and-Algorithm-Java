c7e2fd652ab189d033638ce21a702f358a1be15a
Sunil Bagunji <sunil.bangunji@huntington.com>
2021-05-20 14:10:01 -0400

    Added test cases logic

--- src/algorithms/InvalidParenthesesCount.java
+++ src/algorithms/InvalidParenthesesCount.java
@@ -4,26 +4,43 @@ import java.util.*;
 
 public class InvalidParenthesesCount {
 
-    public static void main(String [] args) {
-        System.out.println(getInvalidParenthesesCount("(())"));
-        System.out.println(getInvalidParenthesesCount("(()("));
-        System.out.println(getInvalidParenthesesCount("((()))"));
-        System.out.println(getInvalidParenthesesCount("()()()"));
-        System.out.println(getInvalidParenthesesCount("))(("));
+    public static void main(String[] args) {
+        HashMap<String, Integer> testCases = new HashMap<>();
+        testCases.put("(())", 0);
+        testCases.put("(()(", 2);
+        testCases.put("((()))", 0);
+        testCases.put("()()()", 0);
+        testCases.put("))((", 4);
+
+        int testCaseCount = 1;
+        int testCasePassed = 1;
+
+        for(Map.Entry<String, Integer> entry : testCases.entrySet()) {
+            boolean result = getInvalidParenthesesCount(entry.getKey()) == entry.getValue();
+            if(result) {
+                testCasePassed++;
+            }
+
+            System.out.println(result
+                    ? testCaseCount++ + " Test case passed!" : testCaseCount++ + " Test case failed - input: (())");
+        }
+        System.out.println();
+        System.out.println(testCasePassed + "/" + testCaseCount + " test cases passed.");
+
     }
 
     private static int getInvalidParenthesesCount(String str) {
-        if(str == null || str.isEmpty()) {
+        if (str == null || str.isEmpty()) {
             return 0;
         }
 
         int count = 0;
-        Stack <Character> stack = new Stack<>();
-        for(char ch : str.toCharArray()) {
-            if(ch == '(') {
+        Stack<Character> stack = new Stack<>();
+        for (char ch : str.toCharArray()) {
+            if (ch == '(') {
                 stack.push(ch);
             } else {
-                if(!stack.isEmpty() && stack.pop() == '(') continue;
+                if (!stack.isEmpty() && stack.pop() == '(') continue;
                 count++;
             }
         }
